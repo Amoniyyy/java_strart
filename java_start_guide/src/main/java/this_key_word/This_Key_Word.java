579acea0d88642e4767f65ee0358f9ac85d07521
Amoniyyy <147065398+Amoniyyy@users.noreply.github.com>
2024-02-08 19:57:43 +0500

    Update This_Key_Word.java

--- java_start_guide/src/main/java/this_key_word/This_Key_Word.java
+++ java_start_guide/src/main/java/this_key_word/This_Key_Word.java
@@ -1,5 +1,16 @@
 package this_key_word;
 
 public class This_Key_Word {
-    
+    public static void main(String[] args) {
+    }
+}
+class Human {
+    int age;
+    String name;
+    public void setOne(int age){
+        this.age = age; // "this" means that it's the object's field
+    }
+    public boolean equals(Human human) {
+        return this == human; // this also means "The created object"
+    }
 }
